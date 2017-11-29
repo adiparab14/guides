160e1ee03c58ceca7d839d0d6c9a3696c1d3d84f
Aditya <adiparab14@gmail.com>
2017-11-29 17:34:58 +0100

    added some info log

--- utils/loopPages.js
+++ utils/loopPages.js
@@ -1,14 +1,17 @@
 const { Observable } = require('rx');
 
 const readDir = require('./readDir');
+const info = require('../utils');
 
 module.exports = function loopPages(handler, dir = 'src/pages') {
   return Observable.from(readDir(dir))
     .flatMap(articleParentDir => {
       const path = `${dir}/${articleParentDir}`;
+      info('reading path/file ${path}', 'greenBright');
       handler(path, articleParentDir);
 
       if (readDir(path)) {
+        info('${path} is a directory so looping again', 'greenBright');
         return loopPages(handler, path);
       }
 
