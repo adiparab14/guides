11e779a726a1e5f64904915121c1375446fa2fc6
Aditya <adiparab14@gmail.com>
2017-11-29 17:41:32 +0100

    Update loopPages.js

--- utils/loopPages.js
+++ utils/loopPages.js
@@ -7,11 +7,11 @@ module.exports = function loopPages(handler, dir = 'src/pages') {
   return Observable.from(readDir(dir))
     .flatMap(articleParentDir => {
       const path = `${dir}/${articleParentDir}`;
-      info('reading path/file ${path}', 'greenBright');
+      info(`reading path/file ${path}`, 'greenBright');
       handler(path, articleParentDir);
 
       if (readDir(path)) {
-        info('${path} is a directory so looping again', 'greenBright');
+        info(`${path} is a directory so looping again`, 'greenBright');
         return loopPages(handler, path);
       }
 
